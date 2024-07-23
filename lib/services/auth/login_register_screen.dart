55b9b73cd97d191651bbaeddd714e99f2f66e4f4
EL Alami Aymane <elalamiaymane97@gmail.com>
2024-07-23 13:28:55 +0100

    fixed login error handling

--- lib/services/auth/login_register_screen.dart
+++ lib/services/auth/login_register_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:scorebuddy/Components/my_button.dart';
 import 'package:scorebuddy/Components/my_textfield.dart';
@@ -24,19 +25,58 @@ class loginOrRegisterState extends State<loginOrRegister> {
     }));
   }
 
-  void login() {
-    final authservice = AuthService();
+  void login() async {
+  final authService = AuthService();
+  if (emailController.text.trim().isNotEmpty &&
+      passwordController.text.trim().isNotEmpty) {
     try {
-      authservice.signInWithEmailAndPassword(
-          emailController.text, passwordController.text);
+      await authService.signInWithEmailAndPassword(
+        emailController.text.trim(),
+        passwordController.text.trim(),
+      );
+      // Navigate to another screen or show success message
+    } on FirebaseAuthException catch (e) {
+      String errorMessage;
+      switch (e.code) {
+        case 'user-not-found':
+          errorMessage = 'No user found for that email.';
+          break;
+        case 'wrong-password':
+          errorMessage = 'Wrong password provided.';
+          break;
+        case 'invalid-email':
+          errorMessage = 'The email address is not valid.';
+          break;
+        default:
+          errorMessage = 'An error occurred. Please try again.';
+      }
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('Login Error'),
+          content: Text(errorMessage),
+        ),
+      );
     } catch (e) {
       showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-                title: Text(e.toString()),
-              ));
+        context: context,
+        builder: (context) => const AlertDialog(
+          title: Text('Login Error'),
+          content: Text('Invalid Credentials'),
+        ),
+      );
     }
+  } else {
+    showDialog(
+      context: context,
+      builder: (context) => const AlertDialog(
+        title: Text('Input Error'),
+        content: Text('Both fields should be filled!'),
+      ),
+    );
   }
+}
+
 
   @override
   Widget build(BuildContext context) {
